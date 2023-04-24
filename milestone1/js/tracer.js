98b42b2d01c2b1b4b13b084ad330e2221341d6b5
JeremyCSUMB <jeshaw@csumb.edu>
2023-04-24 11:47:09 -0700

    Added yields between each operation

--- milestone1/js/tracer.js
+++ milestone1/js/tracer.js
@@ -4,10 +4,8 @@
     
     if (state === undefined) { 
       state = {}
-      state.a = sim.randInt(1, 9)
-      if (state.a == 8) state.a = 10
-      state.b = sim.randInt(1, 9)
-      if (state.b == 8) state.b = 10
+      state.a = sim.randInt(1, 10)
+      state.b = sim.randInt(1, 10)
     }
     
     const code = sim.add(0, 0, new Code(`
@@ -29,18 +27,25 @@
     vars.b = state.b
     yield sim.next('Initialized b')
     code.go(3)
+    yield sim.next('Use the Addition Operator')
     yield sim.ask(vars.a + vars.b)
     code.go(4)
+    yield sim.next('Use the Subtraction Operator')
     yield sim.ask(vars.a - vars.b)
     code.go(5)
+    yield sim.next('Use the Division Operator')
     yield sim.ask(vars.a / vars.b)
     code.go(6)
+    yield sim.next('Use the Floor Dvision Operator')
     yield sim.ask(Math.floor(vars.a / vars.b))
     code.go(7)
+    yield sim.next('Use the Multiplication Operator')
     yield sim.ask(vars.a * vars.b)
     code.go(8)
+    yield sim.next('Use the Modulo Operator')
     yield sim.ask(vars.a % vars.b)
     code.go(9)
+    yield sim.next('Use the Exponent Operator')
     yield sim.ask(vars.a ** vars.b)
     })
     // ]]>
