d60f67d4407ca2e8ee3b2ac6fd89dc007f790d40
JeremyCSUMB <jeshaw@csumb.edu>
2023-04-27 09:47:33 -0700

    Added the vcode for milestone3

--- milestone3/js/tracer.js
+++ milestone3/js/tracer.js
@@ -1,51 +1,31 @@
 //<![CDATA[
-    import { addExercise, Code, Frame } from 'https://horstmann.com/codecheck/script/codecheck_tracer.js'
+    import { addExercise, Code, Frame, Terminal } from 'https://horstmann.com/codecheck/script/codecheck_tracer.js'
     addExercise(function*(sim, state) {
     
     if (state === undefined) { 
       state = {}
-      state.a = sim.randInt(1, 10)
-      state.b = sim.randInt(1, 10)
+      state.num_of_hours = sim.randInt(10, 40)
+      state.hourly_rate = sim.randInt(15, 30)
     }
     
     const code = sim.add(0, 0, new Code(`
-    a = ${state.a}
-    b = ${state.b}
-    print(a + b) #Add Operator
-    print(a - b) #Subtraction Operator
-    print(a / b) #Division Operator
-    print(a // b) #Floor Division Operator
-    print(a * b) #Multiplication Operator
-    print(a % b) #Modulo Operator
-    print(a ** b) #Exponent Operator
+    num_of_hours = ${state.num_of_hours}
+    hourly_rate = ${state.hourly_rate}
+    weekly_wage = num_of_hours * hourly_rate
+    print("Weekly wage = %s", %weekly_wage)
     `))
     const vars = sim.add(0, 5, new Frame())
     yield sim.start(state)
-    vars.a = state.a
-    yield sim.next('Initialized a')
+    vars.num_of_hours = state.num_of_hours
+    yield sim.next('Initialized num_of_hours at ' + state.num_of_hours)
     code.go(2) 
-    vars.b = state.b
-    yield sim.next('Initialized b')
+    vars.hourly_rate = state.hourly_rate
+    yield sim.next('Initialized hourly_rate at ' + state.hourly_rate)
     code.go(3)
-    yield sim.next('Use the Addition Operator')
-    yield sim.ask(vars.a + vars.b)
+    yield sim.ask(vars.num_of_hours * vars.hourly_rate, 'What is the value of weekly_wage')
+    vars.weekly_rate = vars.num_of_hours * vars.hourly_rate
     code.go(4)
-    yield sim.next('Use the Subtraction Operator')
-    yield sim.ask(vars.a - vars.b)
-    code.go(5)
-    yield sim.next('Use the Division Operator')
-    yield sim.ask(vars.a / vars.b)
-    code.go(6)
-    yield sim.next('Use the Floor Dvision Operator')
-    yield sim.ask(Math.floor(vars.a / vars.b))
-    code.go(7)
-    yield sim.next('Use the Multiplication Operator')
-    yield sim.ask(vars.a * vars.b)
-    code.go(8)
-    yield sim.next('Use the Modulo Operator')
-    yield sim.ask(vars.a % vars.b)
-    code.go(9)
-    yield sim.next('Use the Exponent Operator')
-    yield sim.ask(vars.a ** vars.b)
+    const term = sim.add(0, 8, new Terminal())
+    term.println("Weekly wage = " + vars.num_of_hours * vars.hourly_rate)
     })
     // ]]>
