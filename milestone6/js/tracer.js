3da60fb4e9cc3495d070180ca9bbfa69e8bf6fc2
JeremyCSUMB <jeshaw@csumb.edu>
2023-04-28 12:17:58 -0700

    Added code for milestone6

--- milestone6/js/tracer.js
+++ milestone6/js/tracer.js
@@ -1,51 +1,42 @@
 //<![CDATA[
-    import { addExercise, Code, Frame } from 'https://horstmann.com/codecheck/script/codecheck_tracer.js'
-    addExercise(function*(sim, state) {
-    
-    if (state === undefined) { 
-      state = {}
-      state.a = sim.randInt(1, 10)
-      state.b = sim.randInt(1, 10)
-    }
-    
-    const code = sim.add(0, 0, new Code(`
-    a = ${state.a}
-    b = ${state.b}
-    print(a + b) #Add Operator
-    print(a - b) #Subtraction Operator
-    print(a / b) #Division Operator
-    print(a // b) #Floor Division Operator
-    print(a * b) #Multiplication Operator
-    print(a % b) #Modulo Operator
-    print(a ** b) #Exponent Operator
+import { addExercise, Code, Frame, Terminal } from 'https://horstmann.com/codecheck/script/codecheck_tracer.js'
+addExercise(function* (sim) {
+
+  const code = sim.add(0, 0, new Code(`
+    first_num = int(input('Enter the first number: '))
+    second_num = int(input('Enter the second number: '))
+    sum = first_num + second_num
+    if sum >= 22:
+      print('The sum is greater than or equal to 22.')
+    else:
+      print('The sum is less than 22.')
     `))
-    const vars = sim.add(0, 5, new Frame())
-    yield sim.start(state)
-    vars.a = state.a
-    yield sim.next('Initialized a')
-    code.go(2) 
-    vars.b = state.b
-    yield sim.next('Initialized b')
-    code.go(3)
-    yield sim.next('Use the Addition Operator')
-    yield sim.ask(vars.a + vars.b)
-    code.go(4)
-    yield sim.next('Use the Subtraction Operator')
-    yield sim.ask(vars.a - vars.b)
+
+  const vars = sim.add(0, 3, new Frame())
+  const term = sim.add(0, 6, new Terminal())
+
+  yield sim.start()
+  term.print('Enter the first number: ')
+  vars.first_num = yield term.ask(undefined, 'Enter the first number: ')
+  code.go(2)
+  term.print('Enter the second number: ')
+  vars.second_num = yield term.ask(undefined, 'Enter the second number: ')
+  code.go(3)
+  vars.sum = yield sim.ask(parseInt(vars.first_num) + parseInt(vars.second_num), 'Enter the sum: ')
+  code.go(4)
+  yield sim.next('This conditional statement checks whether the sum provided is greater than or equal to 22.')
+  if (vars.sum >= 22) {
     code.go(5)
-    yield sim.next('Use the Division Operator')
-    yield sim.ask(vars.a / vars.b)
-    code.go(6)
-    yield sim.next('Use the Floor Dvision Operator')
-    yield sim.ask(Math.floor(vars.a / vars.b))
+    yield sim.next('In this case, ' + vars.sum + ' is greater than or equal to 22.')
+    term.println('The sum is greater than or equal to 22.')
+    yield sim.next('The code inside the if statement will be executed.')
+  } else {
+    code.go(5)
+    yield sim.next('In this case, ' + vars.sum + ' is less than 22. We will go to our else statement.')
     code.go(7)
-    yield sim.next('Use the Multiplication Operator')
-    yield sim.ask(vars.a * vars.b)
-    code.go(8)
-    yield sim.next('Use the Modulo Operator')
-    yield sim.ask(vars.a % vars.b)
-    code.go(9)
-    yield sim.next('Use the Exponent Operator')
-    yield sim.ask(vars.a ** vars.b)
-    })
+    term.println('The sum is less than 22.')
+    yield sim.next('The code inside our else statement will be executed.')
+  }
+
+})
     // ]]>
